f2e93f314b97a8cea1a655f801f2ec7d42850ed4
reyhanebnyd <bonyadiho@gmail.com>
2025-11-06 19:37:28 +0330

    change database for postgres

--- app/database.py
+++ app/database.py
@@ -1,16 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import settings
 
-# Use SQLite for local dev (simple file database)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
-
-# For SQLite you must add `check_same_thread=False`
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
